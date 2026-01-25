ed265f0a7e48224cfa93383bd9e6e52893551f9d
adetunjibadmus398-droid <adetunjibadmus398@gmail.com>
2026-01-25 10:42:36 -0500

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -1,24 +1,8 @@
 provider "aws" {
     region = "us-east-1"
 }
-variable "az" {
-    type = list
-    default = ["us-east-1a", "us-east-1b", "us-east-1c"]
+resource "aws_instance" "demo" {
+  ami    = "ami-0532be01f26a3de55"
+  instance_type = "t2.micro"
 }
-  resource "aws_elb" "bar" {
-  name               = "foobar-terraform-elb"
-  availability_zones = var.az
- 
-  access_logs {
-    bucket        = "foo"
-    bucket_prefix = "bar"
-    interval      = 60
-  }
- 
-  listener {
-    instance_port     = 8000
-    instance_protocol = "http"
-    lb_port           = 80
-    lb_protocol       = "http"
-  }
-}
+
