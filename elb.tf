1e11f48a0bed67626c6b67147f5d70c69b8fe496
adetunjibadmus398-droid <adetunjibadmus398@gmail.com>
2026-01-27 10:22:33 -0500

    Create elb.tf

---
+++ elb.tf
@@ -0,0 +1,25 @@
+resource "aws_instance" "myec2" {
+  ami           = lookup(var.ami, var.region)
+  instance_type = "t2.micro"
+  count         = 2
+  tags          = { name = element(var.tags,count.index) }
+}
+variable "az" {
+    type = list
+    default = ["us-east-1a", "us-east-1b", "us-east-1c"]
+}
+  resource "aws_elb" "bar" {
+  name               = "foobar-terraform-elb"
+  availability_zones = var.az
+  access_logs {
+    bucket        = "foo"
+    bucket_prefix = "bar"
+    interval      = 60
+  }
+  listener {
+    instance_port     = 8000
+    instance_protocol = "http"
+    lb_port           = 80
+    lb_protocol       = "http"
+  }
+}
